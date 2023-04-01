7e5bddb7c81d23498f9426a39315035516416e81
hyori Choi <hyoring030@gmail.com>
2023-04-01 09:53:37 +0900

    solved

--- task2.js
+++ task2.js
@@ -1,1 +1,21 @@
 /* 2번 문제 답안을 작성해주세요. */
+function solution(num_list) {
+    var answer = [];
+    let even=0;
+    let odd=0;
+    
+    for(let i=0; i< num_list.length; i++){
+        
+    if (num_list[i]%2===0){
+        even++;
+        
+    }
+    else{
+        odd++;
+        
+    }
+    }
+    answer.push(even)
+    answer.push(odd)
+    return answer;
+}
--- task3.js
+++ task3.js
@@ -1,1 +1,17 @@
 /* 3번 문제 답안을 작성해주세요. */
+function fac(n){
+    let a=1;
+    for(let i=1; i<= n;i++){
+    a=a*i;
+    }
+    return a;
+}
+
+function solution(n) {
+    var answer = 0;
+    for(let i=1; fac(i)<=n;i++){   
+    answer=i;
+    }
+
+    return answer;
+}
